753126e74e4e70f8feb5cbd2c328d1951ccfcf4f
Narcis Brindusescu <Narcis75@gmail.com>
2020-11-30 18:42:27 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hoppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sss =6+5;
+            Console.WriteLine("Hello World!"+sss);
+            sss++;
+        }
+    }
+}
